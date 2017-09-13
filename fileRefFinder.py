1cc89c4fae72d81ee5822c850f1fff0f3b1f774d
JakeW <jakewellsd@gmail.com>
2017-09-13 19:19:50 +0100

    Added FileRef Regex
    Added the regex that will be used to determine if a passed string is a file reference or not

--- fileRefFinder.py
+++ fileRefFinder.py
@@ -2,6 +2,17 @@ import pyperclip, re
 
 #CREATE FILE REFERENCE  REGEX
 
+fileRefRegex = re.compile(r'''(
+    (\d{4})                 #First four numbers of reference
+    (CNA|cna|CNS|cns|
+     KRS|krs|VNA|vna|
+     VNS|vns|ICC|icc|       #FileRef
+     BDS|bds|NOV|nov|
+     BCT|bct|CTC|ctc|
+     HTC|htc) 
+    (\d{3})                 # Last 3 digits 
+    (A|B|C)?                # Sometimes file references that are duplicated end in an A/B/C
+    )''', re.VERBOSE)
 
 
 #LOCATE MATCHES IN CLIPBOARD TEXT

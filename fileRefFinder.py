8771f9b5e22ba39f48dff330a29a0d8afc512305
JakeW <jakewellsd@gmail.com>
2017-09-14 17:22:54 +0100

    Added sort function
    Added the sort function. All output is now sorted according to the last 3 digits of each file reference

--- fileRefFinder.py
+++ fileRefFinder.py
@@ -1,5 +1,10 @@
 import pyperclip, re
 
+matches = []
+matchesShortened = []
+matchesSort = []
+matchOutput = []
+
 #CREATE FILE REFERENCE  REGEX
 
 fileRefRegex = re.compile(r'''(
@@ -9,7 +14,7 @@ fileRefRegex = re.compile(r'''(
      VNS|vns|ICC|icc|       #FileRef
      BDS|bds|NOV|nov|
      BCT|bct|CTC|ctc|
-     HTC|htc) 
+     HTC|htc|BDA|bda) 
     (\d{3})                 # Last 3 digits 
     (A|B|C)?                # Sometimes file references that are duplicated end in an A/B/C
     )''', re.VERBOSE)
@@ -17,22 +22,45 @@ fileRefRegex = re.compile(r'''(
 
 #LOCATE MATCHES IN CLIPBOARD TEXT
 
-matches = []
-
 clipboardText = str(pyperclip.paste())
 
 for groups in fileRefRegex.findall(clipboardText):
     fileRef = ''.join([groups[0]])
     matches.append(fileRef)
+    
 
 #SORT FILE REFERENCES BY LAST 3 DIGITS
+    
+for i in matches:
+    t=i
+    t=t[7:11]
+    matchesShortened.append(t)
 
+matchesShortened.sort()
 
+c = 0
+length = len(matches)
+
+for i in matches:
+    while c < length:
+        if i[7:11] == matchesShortened[c]:
+            cNumber = str(c)
+            matchesSort.append(cNumber+ ' ' +i)
+            c=0
+            break
+        else:
+            c+=1
+matchesSort.sort()
+
+for i in matchesSort:
+    t=i
+    t=i[2:15]
+    matchOutput.append(t)
 
 #COPY MATCHES TO THE CLIPBOARD
     
 if len(matches) > 0:
-    pyperclip.copy('\n'.join(matches))
+    pyperclip.copy('\n'.join(matchOutput))
     print('Copied to clipboard:')
 
 else:

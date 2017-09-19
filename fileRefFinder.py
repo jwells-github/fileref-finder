9be4755a44e95731d34919319a5ccf78c95a2ce3
JakeW <jakewellsd@gmail.com>
2017-09-19 21:15:39 +0100

    Added additional comments
    Added comments to better explain the purpose of different for loops

--- fileRefFinder.py
+++ fileRefFinder.py
@@ -5,7 +5,7 @@ matchesShortened = []
 matchesSort = []
 matchOutput = []
 
-#CREATE FILE REFERENCE  REGEX
+#FILE REFERENCE  REGEX
 
 fileRefRegex = re.compile(r'''(
     (\d{4})                 #First four numbers of reference
@@ -19,18 +19,19 @@ fileRefRegex = re.compile(r'''(
     (A|B|C)?                # Sometimes file references that are duplicated end in an A/B/C
     )''', re.VERBOSE)
 
-
 #LOCATE MATCHES IN CLIPBOARD TEXT
 
 clipboardText = str(pyperclip.paste())
 
+#Searches the clipboard text for file references and appends them to the matchesFromClipboard list
 for groups in fileRefRegex.findall(clipboardText):
     fileRef = ''.join([groups[0]])
     matchesFromClipboard.append(fileRef)
     
 
 #SORT FILE REFERENCES BY LAST 3 DIGITS
-    
+
+#Appends the last 3 digits of every file reference to the matchesShortened List    
 for i in matchesFromClipboard:
     t=i
     t=t[7:11]
@@ -38,11 +39,11 @@ for i in matchesFromClipboard:
 
 matchesShortened.sort()
 
-
-
 for i in matchesFromClipboard:
     c = 0
     length = len(matchesFromClipboard)
+    #Compares the full file reference to the sorted, shortened file reference
+    #Appends the list position of the shortened file reference to the beginning of the full file reference
     while c < length:
         if i[7:11] == matchesShortened[c]:
             cNumber = str(c)
@@ -51,8 +52,10 @@ for i in matchesFromClipboard:
             break
         else:
             c+=1
+#Sorts the full file refernce list by the appended list position number
 matchesSort.sort()
 
+#Removes the appended list position number from the start of the full file reference
 for i in matchesSort:
     t=i
     t=i[2:15]
